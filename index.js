db9e88ee30559dafa707f87e6e9843311d38d7bb
Damaera <damaera@live.com>
2018-04-12 22:11:20 +0700

    get related tag

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => res.json({ status: "ok" }));
+
 const route = (param, filename) => {
   app.get(param, require("./routes/" + filename));
 };
 
-app.get("/", (req, res) => res.json({ status: "ok" }));
 route("/post", "post");
+route("/related", "related");
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
---
+++ routes/related.js
@@ -0,0 +1,34 @@
+const api = require("./api");
+
+module.exports = function(req, res) {
+  const { tag } = req.query;
+  if (tag) {
+    api.database
+      .getDiscussions("trending", {
+        tag,
+        limit: 20,
+        truncate_body: 1
+      })
+      .then(collection => {
+        const related = {};
+        collection.forEach(data => {
+          try {
+            const { tags } = JSON.parse(data.json_metadata);
+            tags.forEach(item => {
+              const relatedTag = related[item];
+              if (tag === item) {
+              } else if (!relatedTag) {
+                related[item] = 1;
+              } else {
+                related[item] = relatedTag + 1;
+              }
+            });
+          } catch (e) {}
+        });
+        const relatedSorted = Object.keys(related)
+          .sort((a, b) => related[b] - related[a])
+          .reduce((obj, key) => ({ ...obj, [key]: related[key] }), {});
+        res.json(relatedSorted);
+      });
+  }
+};
